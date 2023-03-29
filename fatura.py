659cea0b98513fc02cad4946924d6b3dc519124a
isaac girao <isaacgirao2013@hotmail.com>
2023-03-29 16:46:49 -0300

    meu primeiro commit

---
+++ fatura.py
@@ -0,0 +1,6 @@
+NOME = input('Qual é o seu nome? ')
+DIA = input('Qual é o dia de vencimento da fatura? ')
+MES = input('Qual é o mês de vencimento da fatura? ')
+VP = input('Qual é o valor da parcela? ')
+
+print("ola sr," +  NOME + " \nsua fatura com o valor de R$ " + VP + b"DO MES DE " + MES + DIA +"ESTA FECHADA"  + "!" )
